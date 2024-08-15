5fd3f94bafecb7e40aa297ace1814034738915aa
likhithkp <likhithkpdev@gmail.com>
2024-08-15 16:47:00 +0530

    Practice

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const getFact = async () => {
     const blob = await fetch(`https://randomuser.me/api/`);
     const parsedData = await blob.json(); 
-    console.log(parsedData.results);
 }
 
 getFact();
